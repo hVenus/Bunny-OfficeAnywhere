958b5af76dd76f4452d11fda535cb9e22afc85ef
hVenus <kevin@fm618.com>
2013-03-23 23:49:54 +0800

    增加了一些文件，程序入口。
    模版，theme,首页等。

---
+++ templates/1/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 首页模版文件
+ * Author: hVenus
+ * Date: 2013/3/23
+ */
+?>
+<html>
+<body>
+<h1>Bunny</h1>
+</body>
+</html>
---
+++ webroot/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * 程序入口
+ * Author: hVenus
+ * Create Date: 2013/3/23
+ * 所有程序的入口。
+ */
+echo "Hello, Bunny.";
