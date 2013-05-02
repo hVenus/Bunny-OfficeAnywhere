b836a3aac700e3de77762d4691d8cb7495482bd9
hVenus <kevin@fm618.com>
2013-05-03 00:55:00 +0800

    使用zend framework做为基础框架

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,37 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Description of AlbumController
+ *
+ * @author keysentence
+ */
+class AlbumController extends AbstractActionController {
+    protected $albumTable;
+    
+    public function getAlbumTable(){
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
+    }
+    
+    public function indexAction(){
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
+    }
+    
+    public function addAction(){}
+    
+    public function editAction(){}
+    
+    public function deleteAction(){}
+    
+}
+
+?>
---
+++ module/Album/src/Album/Model/Album.php
@@ -0,0 +1,21 @@
+<?php
+namespace Album\Model;
+
+/**
+ * Description of Album
+ *
+ * @author keysentence
+ */
+class Album {
+    public $id;
+    public $artist;
+    public $title;
+    
+    public function exchangeArray($data){
+        $this->id = (isset($data['id']))?$data['id']:null;
+        $this->artist = (isset($data['artist']))?$data['artist']:null;
+        $this->title = (isset($data['title']))?$data['title']:null;
+    }
+}
+
+?>
