a4ebd68b452105f2af9b5a427ac63461ae84f812
hVenus <kevin@fm618.com>
2013-05-04 00:35:46 +0800

    add album

---
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -0,0 +1,37 @@
+<?php
+namespace Album\Form;
+
+use Zend\Form\Form;
+
+/**
+ * Description of AlbumForm
+ *
+ * @author keysentence
+ */
+class AlbumForm extends Form {
+    public function __construct($name = null){
+        parent::__construct('album');
+        $this->setAttribute('method', 'post');
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'title',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Title',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'artist',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Go',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
+
+?>
