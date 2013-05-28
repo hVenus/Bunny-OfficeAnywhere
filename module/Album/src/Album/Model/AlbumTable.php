294d7bcabaa13cf0e3f8b5c05f926b306972f2e9
hVenus <kevin@fm618.com>
2013-05-28 23:42:07 +0800

    add paginator

--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -2,7 +2,12 @@
 
 namespace Album\Model;
 
+
+use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Sql\Select;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
 
 /**
  * Description of AlbumTable
@@ -17,7 +22,25 @@ class AlbumTable {
         $this->tableGateway = $tableGateway;
     }
 
-    public function fetchAll() {
+    public function fetchAll($paginated=false) {
+        if($paginated) {
+            // create a new Select object for the table album
+            $select = new Select('album');
+            // create a new result set based on the Album entity
+            $resultSetPrototype = new ResultSet();
+            $resultSetPrototype->setArrayObjectPrototype(new Album());
+            // create a new pagination adapter object
+            $paginatorAdapter = new DbSelect(
+            // our configured select object
+                $select,
+                // the adapter to run it against
+                $this->tableGateway->getAdapter(),
+                // the result set to hydrate
+                $resultSetPrototype
+            );
+            $paginator = new Paginator($paginatorAdapter);
+            return $paginator;
+        }
         $resultSet = $this->tableGateway->select();
         return $resultSet;
     }
