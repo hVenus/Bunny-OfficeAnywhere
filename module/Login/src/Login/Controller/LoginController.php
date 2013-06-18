66f2e47f8c0f79f71e277d9d37f6048681a8d08c
hVenus <kevin@fm618.com>
2013-06-19 00:19:38 +0800

    Login model

--- module/Login/src/Login/Controller/LoginController.php
+++ module/Login/src/Login/Controller/LoginController.php
@@ -1,48 +1,33 @@
 <?php
-/**
- * LoginController.php
- *
- * Created by JetBrains PhpStorm.
- * 
- * Bunny-OfficeAnywhere: PROJECT DESCRIPTION.
- * Copyright (c) 2013, Bunny-OfficeAnywhere All rights reserved.
- * 
- * Copyright (c) <YEAR>, <OWNER>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <ORGANIZATION> nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * @copyright	Copyright 2013, Bunny-OfficeAnywhere.
- * @link		http://your.domain
- * @package		LoginController.php
- * @since		Bunny-OfficeAnywhere v 0.0.0
- * @lisense		BSD-new Lisense (Berkeley Software Distribution license)
- */
+namespace Login\Controller;
 
-namespace login;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Login\Model\Login;
+use Login\Form\LoginForm;
+
+class LoginController extends AbstractActionController {
+
+    public function indexAction(){
+        $form = new LoginForm(); // Login form
+        $form->get('submit')->setValue('GO'); // 设置提交按钮的显示文本
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            // 如果有值传进来
+            $login = new Login();
+            $form->setInputFilter($login->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $login->exchangeArray($form->getData());
 
 
-class LoginController {
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('home');
+            }
+        }
 
+        return array('form' => $form); // 显示Form
+    }
 }
--- module/Login/src/Login/Model/Login.php
+++ module/Login/src/Login/Model/Login.php
@@ -1,41 +1,78 @@
 <?php
-/**
- * Login.php
- *
- * Created by JetBrains PhpStorm.
- * 
- * Bunny-OfficeAnywhere: PROJECT DESCRIPTION.
- * Copyright (c) 2013, Bunny-OfficeAnywhere All rights reserved.
- * 
- * Copyright (c) <YEAR>, <OWNER>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <ORGANIZATION> nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * @copyright	Copyright 2013, Bunny-OfficeAnywhere.
- * @link		http://your.domain
- * @package		Login.php
- * @since		Bunny-OfficeAnywhere v 0.0.0
- * @lisense		BSD-new Lisense (Berkeley Software Distribution license)
- */
+namespace Login\Model;
+
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+
+class Login implements InputFilterAwareInterface {
+
+    public $loginname;
+    public $loginpassword;
+    protected $inputFilter;
+
+    public function exchangeArray($data){
+        $this->loginname = (isset($data['loginname']))?$data['loginname']:null;
+        $this->loginpassword = (isset($data['loginpassword']))?$data['loginpassword']:null;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter){
+        throw new Exception("Not used");
+    }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory     = new InputFactory();
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'loginname',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 128,
+                        ),
+                    ),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'loginpassword',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 128,
+                        ),
+                    ),
+                ),
+            )));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
+
+    public function getArrayCopy(){
+        return get_object_vars($this);
+    }
+}
